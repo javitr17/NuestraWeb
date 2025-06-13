88bfd83d4dc4ade82948c7e9f2a444e71ee70619
javitr17 <javtru17@gmail.com>
2025-06-13 19:37:06 +0200

    render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn weblu.wsgi
--- weblu/settings.py
+++ weblu/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z%xpg&)nvlwut%v5h!rlex#vh945nyr+x$m19p8&&nh27=o-u&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
